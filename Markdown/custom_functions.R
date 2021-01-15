e3940d9b5c9a0657fa84f2d67f20bee74b4cc52a
Fadel Megahed <fmegahed@miamioh.edu>
2021-01-15 09:06:53 -0500

    Updated the data

--- Markdown/custom_functions.R
+++ Markdown/custom_functions.R
@@ -15,3 +15,34 @@ quiet <- function(x) {
   on.exit(sink()) 
   invisible(force(x)) 
 } 
+
+# To be potentially used if we used a MCMC model
+HDIofMCMC = function( sampleVec , credMass=0.95 ) {
+  # Written by John Kruschke, Indiana University
+  # Author of Doing Bayesian Data Analysis, 2e
+  # Available as part of DBDA2E-utilities at
+  #     https://github.com/boboppie/kruschke-doing_bayesian_data_analysis
+  # (This is an excellent book on Bayesian statistics ... SR)
+  
+  # Computes highest density interval from a sample of representative values,
+  #   estimated as shortest credible interval.
+  # Arguments:
+  #   sampleVec
+  #     is a vector of representative values from a probability distribution.
+  #   credMass
+  #     is a scalar between 0 and 1, indicating the mass within the credible
+  #     interval that is to be estimated.
+  # Value:
+  #   HDIlim is a vector containing the limits of the HDI
+  sortedPts = sort( sampleVec )
+  ciIdxInc = ceiling( credMass * length( sortedPts ) )
+  nCIs = length( sortedPts ) - ciIdxInc
+  ciWidth = rep( 0 , nCIs )
+  for ( i in 1:nCIs ) {
+    ciWidth[ i ] = sortedPts[ i + ciIdxInc ] - sortedPts[ i ]
+  }
+  HDImin = sortedPts[ which.min( ciWidth ) ]
+  HDImax = sortedPts[ which.min( ciWidth ) + ciIdxInc ]
+  HDIlim = c( HDImin , HDImax )
+  return( HDIlim )
+}
